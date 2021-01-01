69d0afb5553479b7449aadda2a61026fc174fd01
Luigi Derson <luigi.prodesign@gmail.com>
2021-01-01 22:49:58 +0000

    Project setup

---
+++ src/components/ComingSoon.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ComingSoon = () => {
+  return (
+    <div>
+      <h2>&gt; Comitting changes...</h2>
+      <p>Building a new visual experience</p>
+    </div>
+  )
+}
+
+export default ComingSoon
