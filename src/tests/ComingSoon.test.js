5befeadb2740d7fc61e6b09bc0f5e6c15174cc7e
Luigi Derson <luigi.prodesign@gmail.com>
2021-01-10 22:21:29 +0000

    update test

--- src/tests/ComingSoon.test.js
+++ src/tests/ComingSoon.test.js
@@ -1,7 +1,16 @@
 import renderer from 'react-test-renderer'
 import ComingSoon from 'components/pages/ComingSoon'
 
+import { ThemeProvider } from 'styled-components'
+import { light } from 'components/themes'
+
 test('renders correctly', () => {
-  const tree = renderer.create(<ComingSoon />).toJSON()
+  const tree = renderer
+    .create(
+      <ThemeProvider theme={light}>
+        <ComingSoon />
+      </ThemeProvider>
+    )
+    .toJSON()
   expect(tree).toMatchSnapshot()
 })
